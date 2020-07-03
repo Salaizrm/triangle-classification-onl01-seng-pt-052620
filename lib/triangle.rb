31a68e0b80d8d21e5ef064b89a5e42499557878f
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-03 20:06:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,12 @@ class Triangle
       :isosceles
     elsif x != y && x != z
       :scalene
+    end
 
+    if x <= 0 || y <= 0 || z <= 0
+      raise TriangleError
+    end
+      
     end
 
   end
